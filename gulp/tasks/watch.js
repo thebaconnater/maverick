6a67ef2a80846b4e38d3bbcd30f32822c1982f57
Richard <richardortega13@gmail.com>
2017-01-03 17:28:59 -0500

    Completed Our Features Section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 
 gulp.task('watch', function(){
-   watch('./app/index.html', function() {
-       gulp.start('html'); 
-   });
-
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('styles');
     });
